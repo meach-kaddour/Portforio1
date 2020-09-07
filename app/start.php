78d9e450965b02152bf63fff12c5197ac2becc90
meach <meachkaddour1@gmail.com>
2020-09-07 03:31:35 +0100

    create data base and data base connection

---
+++ app/start.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors','On');
+
+define('APP_ROOT','__DIR__');
+
+define('BASE_ROOT','http://localhost/cms%20porte/');
+
+$db = new PDO('mysql:host=127.0.0.1;dbname=phpcms','root','');
+
+
+?>
